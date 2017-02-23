65efe3341cd68595e9056b13b725e2cec096971e
Hans-Peter Bock <Hans-Peter.Bock@de.trumpf.com>
2017-02-23 13:30:34 +0100

    optimized imports added header

--- src/main/java/iuno/tdm/payment/service/bitcoin/Bitcoin.java
+++ src/main/java/iuno/tdm/payment/service/bitcoin/Bitcoin.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2016 TRUMPF Werkzeugmaschinen GmbH + Co. KG
+ * Created by Hans-Peter Bock on 20.02.2017.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package iuno.tdm.payment.service.bitcoin;
 
 import org.bitcoinj.core.BlockChain;
@@ -11,15 +29,8 @@ import org.bitcoinj.wallet.Wallet;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
-import static java.util.Arrays.asList;
-
-/**
- * Created by bockha on 20.02.17.
- */
 public class Bitcoin {
     final NetworkParameters params = TestNet3Params.get();
 
