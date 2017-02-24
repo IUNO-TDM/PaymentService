aa5aae21a7062d93e4beb5ec61f2cf563a4c322c
Hans-Peter Bock <Hans-Peter.Bock@de.trumpf.com>
2017-02-24 11:36:42 +0100

    bitcoin invoices can now be added

--- src/main/java/io/swagger/api/impl/InvoicesApiServiceImpl.java
+++ src/main/java/io/swagger/api/impl/InvoicesApiServiceImpl.java
@@ -9,11 +9,15 @@ import io.swagger.model.Error;
 import io.swagger.model.Invoice;
 import io.swagger.model.InvoiceId;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import io.swagger.api.NotFoundException;
 
 import java.io.InputStream;
+import java.util.UUID;
 
+import iuno.tdm.paymentservice.Bitcoin;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
 import javax.ws.rs.core.Response;
@@ -21,6 +25,8 @@ import javax.ws.rs.core.SecurityContext;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2017-02-20T13:03:48.706Z")
 public class InvoicesApiServiceImpl extends InvoicesApiService {
+    private final Bitcoin bc = Bitcoin.getInstance();
+
     @Override
     public Response addCouponToInvoice(String invoiceId, Coupon coupon, SecurityContext securityContext) throws NotFoundException {
         // do some magic!
@@ -28,8 +34,28 @@ public class InvoicesApiServiceImpl extends InvoicesApiService {
     }
     @Override
     public Response addInvoice(Invoice invoice, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+        // send service unavailable (503) if bitcoin peergroup io not connected
+        if (false == bc.isRunning()) {
+            Error err = new Error();
+            err.setMessage("Peergroup is unavailable.");
+            return Response.status(503).entity(err).build();
+        }
+
+        URI createdUri = null;
+        try {
+            UUID invoiceID = bc.addInvoice(invoice);
+            createdUri = new URI("http://localhost:8080/v1/invoices/" + invoiceID.toString() + "/");
+            return Response.created(createdUri).entity(invoice).build();
+
+        } catch (IllegalArgumentException e) { // error in invoice
+            Error err = new Error();
+            err.setMessage(e.getMessage());
+            return Response.status(400).entity(err).build();
+
+        } catch (URISyntaxException e) { // should never happen
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
     }
     @Override
     public Response deleteInvoiceById(String invoiceId, SecurityContext securityContext) throws NotFoundException {
--- src/main/java/iuno/tdm/paymentservice/Bitcoin.java
+++ src/main/java/iuno/tdm/paymentservice/Bitcoin.java
@@ -18,12 +18,15 @@
 
 package iuno.tdm.paymentservice;
 
+import io.swagger.model.Invoice;
 import org.bitcoinj.core.BlockChain;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.PeerGroup;
+import org.bitcoinj.net.discovery.DnsDiscovery;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.store.BlockStoreException;
 import org.bitcoinj.store.SPVBlockStore;
+import org.bitcoinj.utils.BriefLogFormatter;
 import org.bitcoinj.wallet.UnreadableWalletException;
 import org.bitcoinj.wallet.Wallet;
 import org.slf4j.LoggerFactory;
@@ -31,6 +34,8 @@ import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class Bitcoin {
@@ -40,17 +45,20 @@ public class Bitcoin {
     private PeerGroup peerGroup = null;
     private Logger logger;
 
+    private HashMap<UUID, BitcoinInvoice> invoiceHashMap = new HashMap<>();
+
     private static final String PREFIX = "PaymentService";
 
     private static Bitcoin instance;
 
     private Bitcoin() {
         logger = LoggerFactory.getLogger(Bitcoin.class);
+        BriefLogFormatter.initWithSilentBitcoinJ();
     }
 
     public static synchronized Bitcoin getInstance() {
         if (Bitcoin.instance == null) {
-            Bitcoin.instance = new Bitcoin ();
+            Bitcoin.instance = new Bitcoin();
         }
         return Bitcoin.instance;
     }
@@ -94,6 +102,7 @@ public class Bitcoin {
         peerGroup.addWallet(wallet);
 
         peerGroup.startAsync();
+        peerGroup.addPeerDiscovery(new DnsDiscovery(params));
     }
 
     public void stop() {
@@ -101,12 +110,35 @@ public class Bitcoin {
         wallet.shutdownAutosaveAndWait();
     }
 
-    public void logStatus() {
+    private void logStatus() {
         logger.info("Balance: " + wallet.getBalance().toFriendlyString());
         logger.info("Estimated: " + wallet.getBalance(Wallet.BalanceType.ESTIMATED).toFriendlyString());
         logger.info("Seed: " + wallet.getKeyChainSeed().getMnemonicCode());
         logger.info("wallet receive address: " + wallet.currentReceiveAddress());
     }
 
-    public boolean isRunning() { return (null != peerGroup); }
+    public boolean isRunning() {
+        return ((null != peerGroup) && (0 < peerGroup.numConnectedPeers()));
+    }
+
+    private class BitcoinInvoice {
+        private Invoice invoice;
+        BitcoinInvoice(Invoice inv) {
+            // check sanity of invoice
+            // check values (transfer shall be lower than totalamount)
+            // expiration date shall be in the future
+            invoice = inv;
+//            throw new IllegalArgumentException("some sanity check failed");
+        }
+    }
+
+    public UUID addInvoice(Invoice inv) {
+        UUID invoiceID = UUID.randomUUID();
+        BitcoinInvoice bcInvoice = new BitcoinInvoice(inv);
+
+        // add invoice to hashMap
+        invoiceHashMap.put(invoiceID, bcInvoice);
+        logger.info("Added invoice " + invoiceID.toString() + " to hashmap.");
+        return invoiceID;
+    }
 }
