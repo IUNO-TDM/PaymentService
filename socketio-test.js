6946579bdfdaed894b66c7d3a71ac1fb43254a44
Hans-Peter Bock <Hans-Peter.Bock@de.trumpf.com>
2018-07-05 14:27:57 +0200

    reduced copy code

--- socketio-test.js
+++ socketio-test.js
@@ -1,94 +1,98 @@
-        console.log('Starting Websocket Client');
+console.log('Starting Websocket Client');
 
-        var invoice = {
-            totalAmount: 100000000,
-            referenceId: 'Brot',
-            expiration: new Date(new Date().getTime() + (2 * 60 * 60 * 1000)).toISOString(),
-            transfers: [{
-                address: 'n2oGNcjsnzB34UYdAvipFoEyR9z4qnLsd5',
-                coin: 99900000
-            }]
-        };
+var invoice = {
+    totalAmount: 100000000,
+    referenceId: 'Brot',
+    expiration: new Date(new Date().getTime() + (2 * 60 * 60 * 1000)).toISOString(),
+    transfers: [{
+        address: 'n2oGNcjsnzB34UYdAvipFoEyR9z4qnLsd5',
+        coin: 99900000
+    }]
+};
 
-        var socket = io('http://localhost:8080/invoices', {
-            transports: ['websocket']
-        });
-        socket.on('connect', function(){
-            console.log('Connected');
-            document.getElementById('log').innerHTML = 'connected';
+var socket = io('http://localhost:8080/invoices', {
+    transports: ['websocket']
+});
+socket.on('connect', function(){
+    console.log('Connected');
+    document.getElementById('log').innerHTML = 'connected';
 
-            var http = new XMLHttpRequest();
-            var url = "http://localhost:8080/v1/invoices";
-            http.open("POST", url, true);
+    var http = new XMLHttpRequest();
+    var url = "http://localhost:8080/v1/invoices";
+    http.open("POST", url, true);
 
-            //Send the proper header information along with the request
-            http.setRequestHeader("Content-type", "application/json");
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/json");
 
-            http.onreadystatechange = function() {//Call a function when the state changes.
-                if(http.readyState === 4 && http.status === 201) {
-                    console.log('Invoice Created:');
-                    console.log(http.responseText);
-                    const jsonData = JSON.parse(http.responseText);
-                    socket.emit('room', jsonData['invoiceId']);
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState === 4 && http.status === 201) {
+            console.log('Invoice Created:');
+            console.log(http.responseText);
+            const jsonData = JSON.parse(http.responseText);
+            socket.emit('room', jsonData['invoiceId']);
 
-                    document.getElementById("invoiceId").innerHTML = jsonData.invoiceId;
+            document.getElementById("invoiceId").innerHTML = jsonData.invoiceId;
 
-                    var request = new XMLHttpRequest();
+            var request = new XMLHttpRequest();
 
-                    request.open("GET","http://localhost:8080/v1/invoices/" + jsonData['invoiceId'] + '/bip21');
-                    request.addEventListener('load', function(event) {
-                        if (request.status >= 200 && request.status < 300) {
-                            console.log(request.responseText);
-                            document.getElementById("qrcode").innerHTML = '';
-                            new QRCode(document.getElementById("qrcode"), request.responseText);
-                            document.getElementById("bip21").innerHTML=request.responseText;
-                        } else {
-                            console.warn(request.statusText, request.responseText);
-                        }
-                    });
-                    request.send();
+            request.open("GET","http://localhost:8080/v1/invoices/" + jsonData['invoiceId'] + '/bip21');
+            request.addEventListener('load', function(event) {
+                if (request.status >= 200 && request.status < 300) {
+                    console.log(request.responseText);
+                    document.getElementById("qrcode").innerHTML = '';
+                    new QRCode(document.getElementById("qrcode"), request.responseText);
+                    document.getElementById("bip21").innerHTML=request.responseText;
+                } else {
+                    console.warn(request.statusText, request.responseText);
                 }
-            };
-            http.send(JSON.stringify(invoice));
-            document.getElementById("referenceId").innerHTML = invoice.referenceId;
+            });
+            request.send();
+        }
+    };
+    http.send(JSON.stringify(invoice));
+    document.getElementById("referenceId").innerHTML = invoice.referenceId;
 
-        });
+});
 
-        socket.on('StateChange', function(data){
-            console.log('StateChange: ' + data);
-            const jd = JSON.parse(data);
-            document.getElementById('qrcode').innerHTML = jd.state;
-            document.getElementById('log').innerHTML = 'StateChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
-            document.getElementById('p-depthInBlocks').innerHTML = jd.depthInBlocks;
-            document.getElementById('p-state').innerHTML = jd.state;
-            document.getElementById('p-bar').style.width = jd.depthInBlocks/6*100 + '%';
-            document.getElementById('testbar').style.width = jd.depthInBlocks/6*100 + '%';
-        });
+function stateChange(txPrefix, data){
+    document.getElementById(txPrefix+'depthInBlocks').innerHTML = data.depthInBlocks;
+    document.getElementById(txPrefix+'state').innerHTML = data.state;
+    document.getElementById(txPrefix+'bar').style.width = data.depthInBlocks/6*100 + '%';
+}
 
-        socket.on('TransferStateChange', function(data){
-            console.log('TransferStateChange: ' + data);
-            const jd = JSON.parse(data);
-            document.getElementById('log').innerHTML = 'TransferStateChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
-            document.getElementById('t-depthInBlocks').innerHTML = jd.depthInBlocks;
-            document.getElementById('t-state').innerHTML = jd.state;
-            document.getElementById('t-bar').style.width = jd.depthInBlocks/6*100 + '%';
-        });
+socket.on('StateChange', function(data){
+    console.log('StateChange: ' + data);
+    const jd = JSON.parse(data);
+    document.getElementById('qrcode').innerHTML = jd.state;
+    document.getElementById('log').innerHTML = 'StateChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
 
-        socket.on('PayingTransactionsChange', function(data){
-            console.log('PayingTransactionsChange: ' + data);
-            const jd = JSON.parse(data);
-            document.getElementById('log').innerHTML = 'PayingTransactionChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
-            document.getElementById('p-txid').innerHTML = jd.transactions[0].transaction;
-        });
+    stateChange('p-', jd);
 
-        socket.on('TransferTransactionsChange', function(data){
-            console.log('TransferTransactionsChange: ' + data);
-            const jd = JSON.parse(data);
-            document.getElementById('log').innerHTML = 'TransferTransactionChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
-            document.getElementById('t-txid').innerHTML = jd.transactions[0].transaction;
-        });
+    document.getElementById('testbar').style.width = jd.depthInBlocks/6*100 + '%';
+});
 
-        socket.on('disconnect', function(){
-            document.getElementById("qrcode").innerHTML = 'disconnected'
-            console.log('Disconnected')
-        });
+socket.on('TransferStateChange', function(data){
+    console.log('TransferStateChange: ' + data);
+    const jd = JSON.parse(data);
+    document.getElementById('log').innerHTML = 'TransferStateChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
+    stateChange('t-', jd);
+});
+
+socket.on('PayingTransactionsChange', function(data){
+    console.log('PayingTransactionsChange: ' + data);
+    const jd = JSON.parse(data);
+    document.getElementById('log').innerHTML = 'PayingTransactionChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
+    document.getElementById('p-txid').innerHTML = jd.transactions[0].transaction;
+});
+
+socket.on('TransferTransactionsChange', function(data){
+    console.log('TransferTransactionsChange: ' + data);
+    const jd = JSON.parse(data);
+    document.getElementById('log').innerHTML = 'TransferTransactionChange: ' + data + '<br>' + document.getElementById('log').innerHTML;
+    document.getElementById('t-txid').innerHTML = jd.transactions[0].transaction;
+});
+
+socket.on('disconnect', function(){
+    document.getElementById("qrcode").innerHTML = 'disconnected'
+    console.log('Disconnected')
+});
