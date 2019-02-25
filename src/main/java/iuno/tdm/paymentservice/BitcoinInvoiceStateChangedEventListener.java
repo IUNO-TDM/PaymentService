f4cfc6f1b524c7a509c49ce540c4d28eda279b40
Hans-Peter Bock <Hans-Peter.Bock@de.trumpf.com>
2019-02-25 15:49:26 +0100

    combined two eventlisteners into one

--- src/main/java/iuno/tdm/paymentservice/BitcoinInvoiceStateChangedEventListener.java
+++ src/main/java/iuno/tdm/paymentservice/BitcoinInvoiceStateChangedEventListener.java
@@ -8,7 +8,8 @@ import org.bitcoinj.core.Transaction;
  * Created by goergch on 08.03.17.
  */
 public interface BitcoinInvoiceStateChangedEventListener {
-    // FIXME combine these two nearly identical listeners into one
-    void onPaymentStateChanged(BitcoinInvoice invoice, State state, Transaction tx, Transactions txList);
-    void onTransferStateChanged(BitcoinInvoice invoice, State state, Transaction tx, Transactions txList);
+    String PAYMENTSTATECHANGE = "PaymentStateChange";
+    String TRANSFERSTATECHANGE = "TransferStateChange";
+
+    void onInvoiceStateChanged(String eventName, BitcoinInvoice invoice, State state, Transaction tx, Transactions txList);
 }
